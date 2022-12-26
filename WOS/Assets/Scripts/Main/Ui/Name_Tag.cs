971a1a88a610eb354a0f8c7cc363b4dc4c41815a
ju-hyeon-kim <63406146+ju-hyeon-kim@users.noreply.github.com>
2022-12-27 04:39:22 +0900

    1227
    window

---
+++ WOS/Assets/Scripts/Main/Ui/Name_Tag.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Name_Tag : MonoBehaviour
+{
+    public Transform myTab;
+    Color UnshowColor = Color.gray;
+
+    Transform Tabs;
+
+    private void Start()
+    {
+        Tabs = myTab.parent;
+    }
+
+    public void ShowTab()
+    {
+        //가장 마지막 자식 오브젝트로 이동함
+        myTab.SetAsLastSibling();
+
+        //형제들은 어둡게 만들고 자신은 밝게 만듬
+        GetComponent<Image>().color = Color.white;
+        Tabs.GetChild(0).GetChild(0).GetComponent<Image>().color = UnshowColor; // 0번째 자식
+        Tabs.GetChild(1).GetChild(0).GetComponent<Image>().color = UnshowColor; // 1번째 자식
+    }
+}
