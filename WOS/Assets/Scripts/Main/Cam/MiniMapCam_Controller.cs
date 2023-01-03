7137cc045d3fd2af76ace1e9bdb8c08916700350
ju-hyeon-kim <63406146+ju-hyeon-kim@users.noreply.github.com>
2023-01-04 03:43:16 +0900

    0104
    minimap

--- WOS/Assets/Scripts/Main/Cam/MiniMapCam_Controller.cs
+++ WOS/Assets/Scripts/Main/Cam/MiniMapCam_Controller.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class MiniMapCam_Controller : MonoBehaviour
 {
     public Transform Cam_Target;
+    public Transform[] Icons;
 
     Vector3 myDir = Vector3.zero;
     float myDist = 0.0f;
+    float minZoom = 40.0f;
+    float maxZoom = 80.0f;
 
     void Start()
     {
@@ -20,16 +23,32 @@ public class MiniMapCam_Controller : MonoBehaviour
     void Update()
     {
         transform.position = Cam_Target.position + myDir * myDist;
-        myDist = Mathf.Clamp(myDist, 30.0f, 50.0f); // ÁÜ Á¦ÇÑ°ª
     }
 
     public void ZoomIn()
     {
-        myDist -= 3.0f;
+        myDist -= 5.0f;
+        myDist = Mathf.Clamp(myDist, minZoom, maxZoom); // ÁÜ Á¦ÇÑ°ª
+        if (myDist > minZoom + 1 && myDist < maxZoom - 1)
+        {
+            for(int i = 0; i < Icons.Length; i++)
+            {
+                Icons[i].localScale = new Vector3(Icons[i].localScale.x * 0.9f, Icons[i].localScale.y * 0.9f, 1);
+            }
+        }
+        
     }
 
     public void ZoomOut()
     {
-        myDist += 3.0f;
+        myDist += 5.0f;
+        myDist = Mathf.Clamp(myDist, minZoom, maxZoom); // ÁÜ Á¦ÇÑ°ª
+        if (myDist > minZoom + 1 && myDist < maxZoom - 1)
+        {
+            for (int i = 0; i < Icons.Length; i++)
+            {
+                Icons[i].localScale = new Vector3(Icons[i].localScale.x * 1.1f, Icons[i].localScale.y * 1.1f, 1);
+            }
+        }
     }
 }
